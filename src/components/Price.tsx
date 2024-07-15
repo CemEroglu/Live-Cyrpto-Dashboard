347e673eba9b2be2993a57a0829e085a5189ec30
Cem Eroglu <cem.eroglu@prodrive-technologies.com>
2024-07-15 22:00:13 +0200

    Flashing price changes is completed.

--- src/components/Price.tsx
+++ src/components/Price.tsx
@@ -1,12 +1,32 @@
-import { baseCurrency } from '../constants'
+import React, { useState, useEffect } from 'react';
+import { baseCurrency } from '../constants';
 import { formatPrice } from '../utils';
+
 interface PriceProps {
   price: number;
 }
 
 function Price({ price }: PriceProps) {
+  const [prevPrice, setPrevPrice] = useState<number | null>(null);
+  const [flashClass, setFlashClass] = useState<string>('');
 
-  return <div>{formatPrice(price)} <span className="base-currency">{baseCurrency}</span></div>;
+  useEffect(() => {
+    if (prevPrice !== null) {
+      if (price > prevPrice) {
+        setFlashClass('price-up');
+      } else if (price < prevPrice) {
+        setFlashClass('price-down');
+      }
+      setTimeout(() => setFlashClass(''), 1000); // Remove class after 1 second
+    }
+    setPrevPrice(price);
+  }, [price]);
+
+  return (
+    <div className={`price ${flashClass}`}>
+      {formatPrice(price)} <span className="base-currency">{baseCurrency}</span>
+    </div>
+  );
 }
 
 export default Price;
